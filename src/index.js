6d904eeeb492d6c1e5cca6d82e2653352b474c7f
iamrupayandaripa <rupayandaripa@gmail.com>
2024-10-27 11:15:37 +0530

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,12 +7,28 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+
+    app.on("error" , (error) => {
+        console.log("ERROR: ", error)
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((e) => {
+    console.log("MONGO DB connection failes!!" , e)
+})
+
 
 
 /*
